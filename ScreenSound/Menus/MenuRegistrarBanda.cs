0edbc8cb96ca809c1a9e00939fafec4bf210b124
eduardoaalmeidaa <almeidaeduardu@gmail.com>
2023-11-25 20:38:22 -0300

    First Commit

---
+++ ScreenSound/Menus/MenuRegistrarBanda.cs
@@ -0,0 +1,28 @@
+﻿using OpenAI_API;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuRegistrarBanda : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        base.Executar(bandasRegistradas);
+        ExibirTituloDaOpcao("Registro das bandas");
+        Console.Write("Digite o nome da banda que deseja registrar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+        Banda banda = new Banda(nomeDaBanda);
+        bandasRegistradas.Add(nomeDaBanda, banda);
+
+        var client = new OpenAIAPI("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");
+        var chat = client.Chat.CreateConversation();
+        chat.AppendSystemMessage($"Resuma a banda {nomeDaBanda} de forma resumida e formal!");
+        string resposta =  chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
+        banda.Resumo = resposta;
+
+        Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+        Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
